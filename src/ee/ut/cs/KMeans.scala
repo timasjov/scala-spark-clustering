94a9978cef3668d97802bf7926e8e56e2d7e08d8
Dmitri Timasjov <timasjov.dmitri@gmail.com>
2014-11-13 22:44:46 +0200

    removed timing in KMeans

--- src/ee/ut/cs/KMeans.scala
+++ src/ee/ut/cs/KMeans.scala
@@ -18,49 +18,42 @@ object KMeans {
     val numberOfClusters = args(1).toInt
     val convergeDistance = args(2).toDouble
 
-    println(time {
-      // take randomly k points
-      val centers = points.takeSample(withReplacement = false, numberOfClusters, System.nanoTime.toInt).toArray
-      var distanceBetweenOldAndNewCentres = 1.0
-      var iterations = 0
+    // take randomly k points
+    val centers = points.takeSample(withReplacement = false, numberOfClusters, System.nanoTime.toInt).toArray
+    var distanceBetweenOldAndNewCentres = 1.0
+    var iterations = 0
 
-      while (distanceBetweenOldAndNewCentres > convergeDistance) {
+    while (distanceBetweenOldAndNewCentres > convergeDistance) {
 
-        // for each point find nearest centre index
-        // return index, point itself and number of reps
-        val closestCenter = points.map(p =>
-          (closestPoint(p, centers), (p, 1))
-        )
+      // for each point find nearest centre index
+      // return index, point itself and number of reps
+      val closestCenter = points.map(p =>
+        (closestPoint(p, centers), (p, 1))
+      )
 
-        // reduce by closest centre
-        // sum all reduces points x, y coordinates and their quantity
-        val aggregatedPoints = closestCenter.reduceByKey {
-          case ((x1, y1), (x2, y2)) => (x1 + x2, y1 + y2)
-        }
-
-        // calculate positions of new centres
-        val newCentres = aggregatedPoints.map { aggregatedPoint =>
-          (aggregatedPoint._1, aggregatedPoint._2._1 * (1.0 / aggregatedPoint._2._2))
-        }.collectAsMap()
-
-        // calculate converge distance between old and new clusters
-        // and assign new centre values to old centres
-        distanceBetweenOldAndNewCentres = 0.0
-        for (i <- 0 until numberOfClusters) {
-          distanceBetweenOldAndNewCentres += squaredDistance(centers(i), newCentres(i))
-          centers(i) = newCentres(i)
-        }
-
-        iterations += 1
+      // reduce by closest centre
+      // sum all reduces points x, y coordinates and their quantity
+      val aggregatedPoints = closestCenter.reduceByKey {
+        case ((x1, y1), (x2, y2)) => (x1 + x2, y1 + y2)
       }
 
-      println("Number of iterations: " + iterations)
-      // println("Centers:")
-      // centers.foreach { c =>
-      //  println(c(0) + ", " + c(1))
-      //}
-    } + " ms")
+      // calculate positions of new centres
+      val newCentres = aggregatedPoints.map { aggregatedPoint =>
+        (aggregatedPoint._1, aggregatedPoint._2._1 * (1.0 / aggregatedPoint._2._2))
+      }.collectAsMap()
 
+      // calculate converge distance between old and new clusters
+      // and assign new centre values to old centres
+      distanceBetweenOldAndNewCentres = 0.0
+      for (i <- 0 until numberOfClusters) {
+        distanceBetweenOldAndNewCentres += squaredDistance(centers(i), newCentres(i))
+        centers(i) = newCentres(i)
+      }
+
+      iterations += 1
+    }
+
+    println("Number of iterations: " + iterations)
     sc.stop()
   }
 
@@ -82,7 +75,7 @@ object KMeans {
     bestIndex
   }
 
-  def time(f: => Unit)={
+  def time(f: => Unit) = {
     val s = System.currentTimeMillis
     f
     System.currentTimeMillis - s
